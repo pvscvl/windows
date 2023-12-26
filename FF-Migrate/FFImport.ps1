87e559bb01c4a57c40e5c974095151f031b17783
pvscvl <pascalschoofs@icloud.com>
2023-12-26 12:06:50 +0100

    Update FFImport.ps1

--- FF-Migrate/FFImport.ps1
+++ FF-Migrate/FFImport.ps1
@@ -1,10 +1,8 @@
-# Define source and destination paths
 $sourcePath = "Q:\"
 $backupFileNamePattern = "FirefoxProfile_*.zip"
-$destinationPath = "C:\temp"  # Specify the destination folder
+$tempPath = "C:\temp"
 $firefoxProfilePath = Join-Path $env:APPDATA "Mozilla\Firefox\Profiles"
 
-# Close Firefox gracefully if it's running
 $firefoxProcesses = Get-Process -Name firefox -ErrorAction SilentlyContinue
 if ($firefoxProcesses) {
     foreach ($process in $firefoxProcesses) {
@@ -16,38 +14,30 @@ if ($firefoxProcesses) {
     }
 }
 
-# Find the most recent backup file matching the pattern
 $latestBackup = Get-ChildItem -Path $sourcePath -Filter $backupFileNamePattern | Sort-Object LastWriteTime -Descending | Select-Object -First 1
 
 if ($latestBackup -ne $null) {
-    # Copy the backup file to the destination folder
-    $destinationFilePath = Join-Path $destinationPath $latestBackup.Name
-    Copy-Item -Path $latestBackup.FullName -Destination $destinationFilePath -Force
 
-    # Get the list of profiles in the Firefox directory
+    $tempFilePath = Join-Path $tempPath $latestBackup.Name
+    Copy-Item -Path $latestBackup.FullName -Destination $tempFilePath -Force
+
     $firefoxProfiles = Get-ChildItem -Path $firefoxProfilePath -Directory
 
-    # Find the most recent profile based on LastWriteTime
     $latestProfile = $firefoxProfiles | Sort-Object LastWriteTime -Descending | Select-Object -First 1
 
     if ($latestProfile -ne $null) {
         $profileFolderPath = $latestProfile.FullName
 
-        # Extract the contents of the backup zip file from the destination folder
-        Expand-Archive -Path $destinationFilePath -DestinationPath $profileFolderPath -Force | Out-Null
+        Expand-Archive -Path $tempFilePath  -DestinationPath $profileFolderPath -Force | Out-Null
 
-        # Remove the .zip file from C:\temp
-        Remove-Item -Path $destinationFilePath -Force
+        Remove-Item -Path $tempFilePath -Force
 
-        # Optional: Display a message to the user
         Write-Host "Firefox profile imported from backup."
 
-        # Check if 64-bit Firefox exists and start it if found
         $firefox64Path = "C:\Program Files\Mozilla Firefox\firefox.exe"
         if (Test-Path $firefox64Path) {
             Start-Process -FilePath $firefox64Path | Out-Null
         } else {
-            # If 64-bit version doesn't exist, check for 32-bit version and start it
             $firefox32Path = "C:\Program Files (x86)\Mozilla Firefox\firefox.exe"
             if (Test-Path $firefox32Path) {
                 Start-Process -FilePath $firefox32Path | Out-Null
