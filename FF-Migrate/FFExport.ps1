9e3e2bd51743bb45cb04ee2f699c597bccd38174
pvscvl <pascalschoofs@icloud.com>
2023-12-26 12:08:02 +0100

    Update FFExport.ps1

--- FF-Migrate/FFExport.ps1
+++ FF-Migrate/FFExport.ps1
@@ -1,4 +1,3 @@
-# Close Firefox gracefully if it's running
 $firefoxProcesses = Get-Process -Name firefox -ErrorAction SilentlyContinue
 if ($firefoxProcesses) {
     foreach ($process in $firefoxProcesses) {
@@ -10,7 +9,6 @@ if ($firefoxProcesses) {
     }
 }
 
-# Define source and destination paths
 $userProfilePath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile)
 $firefoxProfilesPath = Join-Path $userProfilePath "AppData\Roaming\Mozilla\Firefox\Profiles"
 $tempFolderPath = "C:\temp"
@@ -20,23 +18,18 @@ if (-Not (Test-Path -Path $backupPath -PathType Container)) {
     New-Item -Path $backupPath -ItemType Directory -Force | Out-Null
 }
 
-# Find the most recent profile
 $latestProfile = Get-ChildItem $firefoxProfilesPath | Sort-Object LastWriteTime -Descending | Select-Object -First 1
 
 if ($latestProfile -ne $null) {
     $latestProfilePath = $latestProfile.FullName 
 
-    # Change the current directory to the most recent profile folder
     Set-Location -Path $latestProfilePath | Out-Null
 
-    # Create a backup file name based on the profile name and current date
     $backupFileName = "FirefoxProfile_$($latestProfile.Name)_$(Get-Date -Format 'yyyyMMdd').zip"
     $backupFilePath = Join-Path $tempFolderPath $backupFileName
 
-    # Use 7-Zip to compress everything within the profile folder, with "store" compression level
     & "C:\Program Files\7-Zip\7z.exe" a -tzip -mx0 $backupFilePath .\* | Out-Null
 
-    # Copy the zipfile to the destination folder
     Copy-Item -Path $backupFilePath -Destination $backupPath -Force
     Remove-Item -Path $backupFilePath -Force
 
@@ -44,14 +37,11 @@ if ($latestProfile -ne $null) {
     Write-Host "No Firefox profile found in the specified path."
 }
 
-# Part 2: Start Firefox (both 32-bit and 64-bit versions)
 
-# Check if 64-bit Firefox exists and start it if found
 $firefox64Path = "C:\Program Files\Mozilla Firefox\firefox.exe"
 if (Test-Path $firefox64Path) {
     Start-Process -FilePath $firefox64Path | Out-Null
 } else {
-    # If 64-bit version doesn't exist, check for 32-bit version and start it
     $firefox32Path = "C:\Program Files (x86)\Mozilla Firefox\firefox.exe"
     if (Test-Path $firefox32Path) {
         Start-Process -FilePath $firefox32Path | Out-Null
@@ -60,5 +50,4 @@ if (Test-Path $firefox64Path) {
     }
 }
 
-# Optional: Display a message to the user
 Write-Host "Firefox profile backup complete. Firefox has been restarted."
