f1e69a4e27bc3fad3e3940b1343d24572d90f4b7
pvscvl <pascalschoofs@icloud.com>
2024-01-31 23:04:27 +0100

    Update FFExport.ps1

--- FF-Migrate/FFExport.ps1
+++ FF-Migrate/FFExport.ps1
@@ -1,37 +1,43 @@
 param (
 	[switch]$VERBOSE
 )
+
 	$CURRENTDIRECTORY = Get-Location
- 
+	$TEMP_FOLDER_PATH = "C:\temp"
+	$BACKUP_PATH = "Q:\"
+	$7ZIP64EXE = "C:\Program Files\7-Zip\7z.exe"
+	$7ZIP32EXE = "C:\Program Files (x86)\7-Zip\7z.exe"
+	$FIREFOX32EXE = "C:\Program Files (x86)\Mozilla Firefox\firefox.exe"
+	$FIREFOX64EXE = "C:\Program Files\Mozilla Firefox\firefox.exe"
+ 	$USER_PROFILE_PATH = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile)
+	$FIREFOX_PROFILES_PATH = Join-Path $USER_PROFILE_PATH "AppData\Roaming\Mozilla\Firefox\Profiles"
+ 	$LATEST_PROFILE = Get-ChildItem $FIREFOX_PROFILES_PATH | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+  
 	Write-Host "Closing Firefox processes ..."
-$FIREFOX_PROCESSES = Get-Process -Name firefox -ErrorAction SilentlyContinue
+	$FIREFOX_PROCESSES = Get-Process -Name firefox -ErrorAction SilentlyContinue
 	If ($VERBOSE) { Write-Host "Retrieved Firefox processes." }
 
-if ($FIREFOX_PROCESSES) {
-	foreach ($PROCESS in $FIREFOX_PROCESSES) {
-        	$PROCESS.CloseMainWindow() | Out-Null
-        		If ($VERBOSE) { Write-Host "Requested to close Firefox main window for process ID $($PROCESS.Id)." }
-		$PROCESS.WaitForExit(10) | Out-Null
-			If ($VERBOSE) { Write-Host "Waited for process ID $($PROCESS.Id) to exit." }
+	if ($FIREFOX_PROCESSES) {
+		foreach ($PROCESS in $FIREFOX_PROCESSES) {
+        		$PROCESS.CloseMainWindow() | Out-Null
+        			If ($VERBOSE) { Write-Host "Requested to close Firefox main window for process ID $($PROCESS.Id)." }
+			$PROCESS.WaitForExit(10) | Out-Null
+				If ($VERBOSE) { Write-Host "Waited for process ID $($PROCESS.Id) to exit." }
    
-        	if (!$PROCESS.HasExited) {
-            		$PROCESS | Stop-Process -Force | Out-Null
+        		if (!$PROCESS.HasExited) {
+            			$PROCESS | Stop-Process -Force | Out-Null
             			If ($VERBOSE) { Write-Host "Forcefully stopped process ID $($PROCESS.Id)." }
-        	}
-    	}
-}
+        		}
+    		}
+	}
 
 	Write-Host "Exporting Firefox Profile to vol Q."
-$USER_PROFILE_PATH = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile)
+#	$USER_PROFILE_PATH = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::UserProfile)
 	If ($VERBOSE) { Write-Host "User profile path: $USER_PROFILE_PATH" }
 
-$FIREFOX_PROFILES_PATH = Join-Path $USER_PROFILE_PATH "AppData\Roaming\Mozilla\Firefox\Profiles"
+#	$FIREFOX_PROFILES_PATH = Join-Path $USER_PROFILE_PATH "AppData\Roaming\Mozilla\Firefox\Profiles"
 	If ($VERBOSE) { Write-Host "Firefox profiles path: $FIREFOX_PROFILES_PATH" }
 
-$TEMP_FOLDER_PATH = "C:\temp"
-$BACKUP_PATH = "Q:\"
-$7ZIP64EXE = "C:\Program Files\7-Zip\7z.exe"
-$7ZIP32EXE = "C:\Program Files (x86)\7-Zip\7z.exe"
 
 if (-Not (Test-Path -Path $BACKUP_PATH -PathType Container)) {
 	New-Item -Path $BACKUP_PATH -ItemType Directory -Force | Out-Null
@@ -43,7 +49,7 @@ if (-Not (Test-Path -Path $TEMP_FOLDER_PATH -PathType Container)) {
 	If ($VERBOSE) { Write-Host "Created temporary folder path: $TEMP_FOLDER_PATH" }
 }
 
-$LATEST_PROFILE = Get-ChildItem $FIREFOX_PROFILES_PATH | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+#	$LATEST_PROFILE = Get-ChildItem $FIREFOX_PROFILES_PATH | Sort-Object LastWriteTime -Descending | Select-Object -First 1
 	If ($VERBOSE) { Write-Host "Retrieved latest Firefox profile." }
 	If ($VERBOSE) { Write-Host "LATEST_PROFILE: $LATEST_PROFILE" }
 if ($LATEST_PROFILE -ne $null) {
@@ -55,6 +61,7 @@ if ($LATEST_PROFILE -ne $null) {
 
 	$BACKUP_FILE_NAME = "FirefoxProfile_$($LATEST_PROFILE.Name)_$(Get-Date -Format 'yyyyMMdd').zip"
 	$BACKUP_FILE_PATH = Join-Path $TEMP_FOLDER_PATH $BACKUP_FILE_NAME
+ 
 		If ($VERBOSE) { Write-Host "Backup file name and path set." }
 		If ($VERBOSE) { Write-Host "BACKUP_FILE_NAME: $BACKUP_FILE_NAME" }
 		If ($VERBOSE) { Write-Host "BACKUP_FILE_PATH: $BACKUP_FILE_PATH" }
@@ -83,15 +90,13 @@ if ($LATEST_PROFILE -ne $null) {
 	Write-Host "No Firefox profile found in the specified path."
 }
 
-$FIREFOX_64_PATH = "C:\Program Files\Mozilla Firefox\firefox.exe"
-if (Test-Path $FIREFOX_64_PATH) {
-	Start-Process -FilePath $FIREFOX_64_PATH | Out-Null
+if (Test-Path $FIREFOX64EXE) {
+	Start-Process -FilePath $FIREFOX64EXE | Out-Null
 		If ($VERBOSE) { Write-Host "Started 64-bit Firefox." }
-} else {
-	$FIREFOX_32_PATH = "C:\Program Files (x86)\Mozilla Firefox\firefox.exe"
-    	if (Test-Path $FIREFOX_32_PATH) {
-        	Start-Process -FilePath $FIREFOX_32_PATH | Out-Null
-        		If ($VERBOSE) { Write-Host "Started 32-bit Firefox." }
+} 
+elseif { (Test-Path $FIREFOX32EXE) {
+        Start-Process -FilePath $FIREFOX32EXE | Out-Null
+		If ($VERBOSE) { Write-Host "Started 32-bit Firefox." }
 	} else {
 		Write-Host "Firefox not found in the expected locations."
     	}
